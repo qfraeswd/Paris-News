1f533353ef2d8fa5bb30f6e13844c9d470774af9
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-09 17:34:17 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,196 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "%";
+
+client.on("message", async message => {
+    var command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+        if(!message.channel.guild) return;
+            var args = message.content.split(" ").slice(1).join(" ");
+            if(command == "bc") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.forEach(member => {
+                                            let bc = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bc);
+                                        });
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
+                        });
+                            
+                        });
+            }
+            if(command == "bco") {
+                if(!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                }
+                    if(!args) {
+                        return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+                    }
+                        message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟\nمحتوى البرودكاست: \`${args}\`**`).then(m => {
+                            m.react("✅")
+                            .then(() => m.react("❌"));
+
+                            let yesFilter = (reaction, user) => reaction.emoji.name == "✅" && user.id == message.author.id;
+                            let noFiler = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
+
+                            let yes = m.createReactionCollector(yesFilter);
+                            let no = m.createReactionCollector(noFiler);
+
+                            yes.on("collect", v => {
+                                m.delete();
+                                    message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
+                                        message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
+                                            let bco = new Discord.RichEmbed()
+                                            .setColor("RANDOM")
+                                            .setThumbnail(message.author.avatarURL)
+                                            .setTitle("Broadcast")
+                                            .addField("Server", message.guild.name)
+                                            .addField("Sender", message.author.username)
+                                            .addField("Message", args);
+
+                                            member.sendEmbed(bco);
+                                        });
+                        });
+                        no.on("collect", v => {
+                            m.delete();
+                            message.channel.send("**Broadcast Canceled.**").then(msg => msg.delete(3000));
+                        });
+                            
+                        });
+            }
+});
+
+client.on("message", message => {
+    var prefix = "%";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bcs') {
+        if (!args[1]) {
+    message.channel.send("**bcs <message> **");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+    });
+
+client.on("message", async message => {
+    if(message.content.startsWith(prefix + "help")) {
+        let help = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setThumbnail(message.author.avatarURL)
+            .setDescription(`╔════════════════════════════════════╗
+			║             :radio: :microphone2: ***__بـــوت اعــلانــات بــاريـــس__***:radio: :microphone2: 
+			║
+			║▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+			║                                   :shield: **__اوامـــــــر بــــوت__**:shield: 
+			║▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+			║ 
+			║**
+			║1-bc ⟹ برودكاست كــل باســمــك +صــورتك
+			║
+			║2-bco ⟹ برودكاست وان الايـــن فــقــط
+			║
+			║3-bcs ⟹ برودكاست بــدون اســمــك ولا صــور 
+			║**
+			╚════════════════════════════════════╝`);
+            message.channel.sendEmbed(help); 
+    }
+});
+
+const aprefix ="%";
+const dev = ["564414567946387487"];
+client.on('message', message => {
+    var arg = message.content.split(` `).slice(1).join(' ');
+      if (!dev.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(aprefix + 'setg')) { 
+    client.user.setGame(arg);
+      message.channel.send(`** تـم تـغـيـر حـالـه الـى : ${arg} :red_circle:**`)
+  } else 
+  if (message.content.startsWith(aprefix + 'setw')) {
+  client.user.setActivity(arg, {type:'WATCHING'});
+      message.channel.send(`** تـم تـغـيـر حـالـه الـى : ${arg} :black_circle:**`)
+  } else 
+  if (message.content.startsWith(aprefix + 'setl')) {
+  client.user.setActivity(arg , {type:'LISTENING'});
+      message.channel.send(`** تـم تـغـيـر حـالـه الـى : ${arg} :pen_ballpoint: **`)
+  } else 
+  if (message.content.startsWith(aprefix + 'stream')){
+    client.user.setGame(arg, "https://www.twitch.tv/bot");
+      message.channel.send(`**تـم تـغـيـر حـالـه الـى : ${arg} :large_blue_circle:**`)
+  } else
+  if (message.content.startsWith(aprefix + 'setname')) { //لتغير اسم البوت 
+  client.user.setUsername(arg).then
+      message.channel.send(`** تـم تـغـيـر اسـم الـى : ${arg} :radio_button: **`)
+} else
+
+if (message.content.startsWith(aprefix + 'setavatar')) { //لتغير صورة البوت 
+  client.user.setAvatar(arg);
+    message.channel.send(`** تــم تـغـيـر صـور الـى : `` ${arg} ``:camera_with_flash:`);
+} else
+
+if (message.content.startsWith(aprefix + 'setonline')) { //لتغير حالت البوت لي  online
+client.user.setStatus("online")
+    message.channel.send(`**تــم تـغـيـر حـالـه الـى : online :white_circle:**`);
+} else
+    
+if (message.content.startsWith(aprefix + 'setdnd')) { //~~~ dnd
+client.user.setStatus("dnd")
+    message.channel.send(`**تــم تـغـيـر حـالـه الـى : dnd **`);
+} else
+    
+if (message.content.startsWith(aprefix + 'setidle')) { //~~~ idle
+client.user.setStatus("idle")
+    message.channel.send(`**تــم تـغـيـر حـالـه الـى : idle :accept:**`);
+} else
+
+if (message.content.startsWith(aprefix + 'setoffline')) { //~~~ offline
+client.user.setStatus("offline")
+    message.channel.send(`**تــم تـغـيـر حـالـه الـى : offline :o:**`);
+}
+});
+  
+client.login(process.env.BOT_TOKEN);
