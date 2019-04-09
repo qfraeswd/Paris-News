0e8aac6fa319f35f69b549d2049f0604a77362af
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-09 22:07:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,7 +144,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id == 564414567946387487) return;
+  if (message.author.id == 410835593451405312) return;
 
 
 if (message.content.startsWith(prefix + 'playing')) {
