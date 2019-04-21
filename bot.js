0f37c64d56a29df5e64063091099bf985479bda0
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-21 13:14:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -329,4 +329,31 @@ client.on('message',async message => {
             }
 })
 
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+if(message.content ===  'f!color 100') {
+if(message.member.hasPermission('MANAGE_ROLES')) {
+setInterval(function(){})
+message.channel.send('جاري عمل الالوان |✅')
+}else{
+message.channel.send('ما معاك البرمشن المطلوب  |❌')
+}
+}
+});
+
+client.on('message', message=>{
+if (message.content ===  'f!color 100'){
+if(!message.channel.guild) return;
+if (message.member.hasPermission('MANAGE_ROLES')){
+  setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+for(let x = 1; x < 101; x++){
+message.guild.createRole({name:x,
+color: 'RANDOM'})
+}
+}
+}
+});
+
 client.login(process.env.BOT_TOKEN);
